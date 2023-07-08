c73b7416c7d3fd24ee446790750febb3761be95b
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-08 15:33:12 +0300

    create todo model, db.sqlite

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    content = models.CharField(max_length=255)
+    created_time = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tags, related_name="todos")
+
+    class Meta:
+        ordering = ["status", "-created_time"]
