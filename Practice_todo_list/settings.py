20218cd8c9d383f384b4bd0f71e57aea1d38e8c4
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-08 15:22:09 +0300

    create todo app

--- Practice_todo_list/settings.py
+++ Practice_todo_list/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "todo",
+    "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
