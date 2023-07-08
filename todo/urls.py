23eaffc2baab248b19f3fed52a1a1c720bf48fb9
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-08 16:05:31 +0300

    create Tags CRUD-view urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from todo.views import index
+from todo.views import (
+    index,
+    TagsUpdateView,
+    TagsDeleteView,
+    TagsCreateView,
+    TagsListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagsListView.as_view(), name="tags-list"),
+    path("tags/create/", TagsCreateView.as_view(), name="tags-create"),
+    path("tags/<int:pk>/delete/", TagsDeleteView.as_view(), name="tags-delete"),
+    path("tags/<int:pk>/update/", TagsUpdateView.as_view(), name="tags-update"),
 ]
 
 app_name = "todo"
