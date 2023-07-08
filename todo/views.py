c4bbec30d7d7e3ca38cffd14d55cbbbb246ea6c4
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-08 16:02:51 +0300

    create Tags CRUD-view

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-from todo.models import Todo
+from todo.models import Todo, Tags
 
 
 def index(request):
@@ -13,3 +15,28 @@ def index(request):
         "num_toda_notdone": num_toda_notdone
     }
     return render(request, "index.html", context=context)
+
+
+class TagsListView(generic.ListView):
+    model = Tags
+    template_name = "tags/tags_list.html"
+
+
+class TagsCreateView(generic.CreateView):
+    model = Tags
+    fields = "__all__"
+    template_name = "tags/create_update.html"
+    success_url = reverse_lazy("todo:tags-list")
+
+
+class TagsUpdateView(generic.UpdateView):
+    model = Tags
+    fields = "__all__"
+    template_name = "tags/create_update.html"
+    success_url = reverse_lazy("todo:tags-list")
+
+
+class TagsDeleteView(generic.DeleteView):
+    model = Tags
+    template_name = "tags/tags_confirm_delete.html"
+    success_url = reverse_lazy("todo:tags-list")
